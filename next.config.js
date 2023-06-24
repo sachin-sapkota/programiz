a1dbd33b9ce3fcd7c23c4a58cfc6c0c78566501d
sachin sapkota <sachinsapkota773@gmail.com>
2023-06-24 18:25:31 +0545

    final commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { domains: ['storage.googleapis.com'] },
+  eslint: { ignoreDuringBuilds: true },
 };
 
 module.exports = nextConfig;
